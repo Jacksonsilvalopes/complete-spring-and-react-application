5de9158372f9447868f9a96778f2e54659deb089
Jackson Silva Lopes <j.a.ckson@hotmail.com>
2025-01-20 09:44:45 -0300

    implementing template layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
 import Head from "next/head";
+import { Layout } from "components";
 
-
-const Home: React.FC = () =>{
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
         <title>Vendas App</title>
-       
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     Bem Vindo !
-   
+
+      <Layout />
+      Bem vindo !
+     
     </div>
-  )
+  );
+};
 
-}
-
-export default  Home;
-  
-
+export default Home;
